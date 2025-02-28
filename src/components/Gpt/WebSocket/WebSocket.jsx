a57f47c2a02f40f76b782f7313f6c426cbf7b73f
rahul9369 <rp25082003@gmail.com>
2025-02-28 11:56:45 +0530

    remove english

--- src/components/Gpt/WebSocket/WebSocket.jsx
+++ src/components/Gpt/WebSocket/WebSocket.jsx
@@ -42,7 +42,7 @@ import fetchWrapper from "../../../utils/fetchWrapper";
 import { useAuthState } from "../../../hooks/useAuthState";
 
 const languageArr = [
-  "English",
+  // "English",
   "Hindi",
   "Bengali",
   "Punjabi",
@@ -919,8 +919,7 @@ const WebSocketComponent = () => {
         {promptsArr.length > 0 ? (
           <div
             ref={divRef}
-            className="h-[95%] overflow-auto flex flex-col gap-2"
-          >
+            className="h-[95%] overflow-auto flex flex-col gap-2">
             {promptsArr.map((x, index) => (
               <div key={index}>
                 <div className="flex items-center justify-end">
@@ -928,8 +927,7 @@ const WebSocketComponent = () => {
                     <>
                       <IconButton
                         onClick={handleMenuOpen}
-                        sx={{ color: "white" }}
-                      >
+                        sx={{ color: "white" }}>
                         <MoreVert />
                       </IconButton>
                       <Menu
@@ -937,8 +935,7 @@ const WebSocketComponent = () => {
                         anchorEl={anchorElMenu}
                         keepMounted
                         open={Boolean(anchorElMenu)}
-                        onClose={handleMenuClose}
-                      >
+                        onClose={handleMenuClose}>
                         <MenuItem>Reply</MenuItem>
                       </Menu>
                     </>
@@ -952,8 +949,7 @@ const WebSocketComponent = () => {
                       style={{
                         background: x.isUser ? "transparent" : "#303030",
                         borderColor: x.isUser ? "transparent" : "#018081",
-                      }}
-                    >
+                      }}>
                       {(textLoading || translationLoading) &&
                       editIndex == index ? (
                         <div className="h-full w-full p-3 flex flex-col gap-1">
@@ -974,8 +970,7 @@ const WebSocketComponent = () => {
                           key={index}
                           dangerouslySetInnerHTML={{
                             __html: x.text,
-                          }}
-                        >
+                          }}>
                           {/* {x.text} */}
                         </p>
                       )}
@@ -996,8 +991,7 @@ const WebSocketComponent = () => {
                                         ? "none"
                                         : "auto",
                                   }}
-                                  className="m-0 border-2 border-white text-white rounded-lg py-1 px-3 cursor-pointer max-w-[7.5rem] flex justify-center items-center bg-[#018081] hover:bg-opacity-75"
-                                >
+                                  className="m-0 border-2 border-white text-white rounded-lg py-1 px-3 cursor-pointer max-w-[7.5rem] flex justify-center items-center bg-[#018081] hover:bg-opacity-75">
                                   {casesLoading ? (
                                     <CircularProgress
                                       size={15}
@@ -1017,8 +1011,7 @@ const WebSocketComponent = () => {
                                         promptsArr[index - 1].text
                                     );
                                   }}
-                                  className="m-0 border-2 border-white text-white max-w-[7rem] rounded-lg py-1 px-3 cursor-pointer flex justify-center items-center bg-[#018081] hover:bg-opacity-75"
-                                >
+                                  className="m-0 border-2 border-white text-white max-w-[7rem] rounded-lg py-1 px-3 cursor-pointer flex justify-center items-center bg-[#018081] hover:bg-opacity-75">
                                   {relevantCaseLoading ? (
                                     <CircularProgress
                                       size={15}
@@ -1030,8 +1023,7 @@ const WebSocketComponent = () => {
                                 </p>
                                 <p
                                   onClick={handleShowSupremeCourtJudgements}
-                                  className="m-0 border-2 border-white text-white max-w-[9rem] rounded-lg py-1 px-3 cursor-pointer flex justify-center items-center bg-[#018081] hover:bg-opacity-75"
-                                >
+                                  className="m-0 border-2 border-white text-white max-w-[9rem] rounded-lg py-1 px-3 cursor-pointer flex justify-center items-center bg-[#018081] hover:bg-opacity-75">
                                   {supremeCourtLoading ? (
                                     <CircularProgress
                                       size={15}
@@ -1058,8 +1050,7 @@ const WebSocketComponent = () => {
                                   className="flex items-center gap-1 cursor-pointer "
                                   onClick={() =>
                                     handleRegenerateResponse(index)
-                                  }
-                                >
+                                  }>
                                   <img
                                     className="w-4 h-4"
                                     src={regenerateIcon}
@@ -1084,8 +1075,7 @@ const WebSocketComponent = () => {
                                     className="flex items-center gap-1 cursor-pointer"
                                     onClick={() =>
                                       handleRegenerateResponse(index)
-                                    }
-                                  >
+                                    }>
                                     <img
                                       className="w-4 h-4 rounded-none"
                                       src={translateIcon}
@@ -1094,8 +1084,7 @@ const WebSocketComponent = () => {
                                       className="m-0 max-w-fit hover:text-white"
                                       onClick={(e) =>
                                         handleTranslateClick(e, index)
-                                      }
-                                    >
+                                      }>
                                       Translate
                                     </p>
                                   </div>
@@ -1108,8 +1097,7 @@ const WebSocketComponent = () => {
                                     anchorOrigin={{
                                       vertical: "bottom",
                                       horizontal: "left",
-                                    }}
-                                  >
+                                    }}>
                                     {languageArr.sort().map((x, i) => (
                                       <p
                                         onClick={(e) => {
@@ -1117,8 +1105,7 @@ const WebSocketComponent = () => {
                                           handleTranslatePrompt(x);
                                         }}
                                         key={i}
-                                        className="m-0 text-[#018081] py-1 px-4 cursor-pointer border-b border-[#018081]"
-                                      >
+                                        className="m-0 text-[#018081] py-1 px-4 cursor-pointer border-b border-[#018081]">
                                         {x}
                                       </p>
                                     ))}
@@ -1144,8 +1131,7 @@ const WebSocketComponent = () => {
                                   viewBox="0 0 24 24"
                                   className="cursor-pointer"
                                   onClick={() => handleAudioPlayerClick(index)}
-                                  fill="white"
-                                >
+                                  fill="white">
                                   <path d="M19 7.358v15.642l-8-5v-.785l8-9.857zm3-6.094l-1.548-1.264-3.446 4.247-6.006 3.753v3.646l-2 2.464v-6.11h-4v10h.843l-3.843 4.736 1.548 1.264 18.452-22.736z" />
                                 </svg>
                               )}
@@ -1191,8 +1177,7 @@ const WebSocketComponent = () => {
                       <p
                         onClick={() => setInputText(x)}
                         className="border-2 border-gray-400 rounded p-2 cursor-pointer m-0 w-full hover:border-white hover:text-white"
-                        key={index}
-                      >
+                        key={index}>
                         {x}
                       </p>
                     ))}
@@ -1214,15 +1199,13 @@ const WebSocketComponent = () => {
                         background: "white",
                         borderRadius: "4px",
                       }}
-                      size="small"
-                    >
+                      size="small">
                       <Select
                         value={courtName}
                         onChange={handleHighCourtChange}
                         displayEmpty
                         autoWidth
-                        inputProps={{ "aria-label": "Without label" }}
-                      >
+                        inputProps={{ "aria-label": "Without label" }}>
                         <MenuItem disabled value="">
                           <em>Select a High Court</em>
                         </MenuItem>
@@ -1243,8 +1226,7 @@ const WebSocketComponent = () => {
                       gap: 5,
                       marginTop: 5,
                       padding: "0px 10px",
-                    }}
-                  >
+                    }}>
                     <>
                       {relatedCases.cases
                         .slice(0, caseCount)
@@ -1269,8 +1251,7 @@ const WebSocketComponent = () => {
                           onClick={() =>
                             setCaseCount((caseCount) => caseCount + 2)
                           }
-                          className="m-0 border-2 border-white text-white rounded-lg py-1 px-3 cursor-pointer max-w-fit flex justify-center items-center"
-                        >
+                          className="m-0 border-2 border-white text-white rounded-lg py-1 px-3 cursor-pointer max-w-fit flex justify-center items-center">
                           Load More...
                         </p>
                       </div>
@@ -1309,8 +1290,7 @@ const WebSocketComponent = () => {
                       className="text-sm"
                       dangerouslySetInnerHTML={{
                         __html: refRelevantCase,
-                      }}
-                    ></div>
+                      }}></div>
                   ) : (
                     <div className="h-full w-full p-3 flex flex-col gap-1">
                       <div className="w-full h-2 bg-slate-600 animate-pulse  rounded-full"></div>
@@ -1334,8 +1314,7 @@ const WebSocketComponent = () => {
                     display: "flex",
                     flexDirection: "column",
                     gap: "12px",
-                  }}
-                >
+                  }}>
                   <div className="flex justify-between items-center">
                     <p className="font-bold m-0 text-2xl text-white">
                       Reference to Supreme Court Judgements
@@ -1346,8 +1325,7 @@ const WebSocketComponent = () => {
                       className="text-sm"
                       dangerouslySetInnerHTML={{
                         __html: refSupremeCase,
-                      }}
-                    >
+                      }}>
                       {/* <ReactMarkdown>{refSupremeCase}</ReactMarkdown> */}
                       {/* {refSupremeCase}/ */}
                     </div>
@@ -1379,8 +1357,7 @@ const WebSocketComponent = () => {
             });
             setMessagesArray(e);
           }}
-          className="flex gap-2 w-full"
-        >
+          className="flex gap-2 w-full">
           <input
             required
             placeholder="Add your query..."
@@ -1398,8 +1375,7 @@ const WebSocketComponent = () => {
               borderRadius: 10,
               cursor: "pointer",
               marginRight: "5px",
-            }}
-          >
+            }}>
             <FileUploadIcon
               style={{ color: "white", backgroundColor: "transparent" }}
             />
@@ -1414,8 +1390,7 @@ const WebSocketComponent = () => {
             anchorOrigin={{
               vertical: "bottom",
               horizontal: "left",
-            }}
-          >
+            }}>
             {!fileDialog ? (
               <div className="p-3 bg-[#C2FFFF] w-full border-4 border-[#018081]">
                 <div className="flex w-full justify-between items-center gap-28">
@@ -1436,8 +1411,7 @@ const WebSocketComponent = () => {
                     margin="normal"
                     size="small"
                     value={selectedLanguage}
-                    onChange={handleChange}
-                  >
+                    onChange={handleChange}>
                     {multilingualSupportLanguages.sort().map((option) => (
                       <MenuItem key={option} value={option}>
                         {option}
@@ -1452,8 +1426,7 @@ const WebSocketComponent = () => {
                     className="rounded-lg"
                     style={{
                       background: "linear-gradient(90deg,#018081,#001B1B)",
-                    }}
-                  >
+                    }}>
                     Continue
                   </button>
                 </div>
@@ -1501,8 +1474,7 @@ const WebSocketComponent = () => {
           <button
             disabled={inputText === ""}
             type="submit"
-            className="rounded-lg"
-          >
+            className="rounded-lg">
             <SendIcon />
           </button>
         </form>
